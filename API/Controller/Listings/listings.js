839b320e67dee7c25ff1ff395fb66eb8e14c1863
Rehan Arshad <rehanarshad@Rehans-MacBook-Air.local>
2024-07-08 19:52:38 -0500

    added remaining crud api endpoints

--- API/Controller/Listings/listings.js
+++ API/Controller/Listings/listings.js
@@ -1,5 +1,6 @@
 const Listing = require("../../Models/Listings");
 
+// GET ALL LISTINGS
 const getAllListings = async (req, res) => {
   try {
     const allListings = await Listing.find();
@@ -15,6 +16,7 @@ const getAllListings = async (req, res) => {
   }
 };
 
+// ADD NEW LISTING
 const addListing = (req, res) => {
   try {
     const addListing = new Listing(req.body);
@@ -37,7 +39,64 @@ const addListing = (req, res) => {
   }
 };
 
+// GET SPECIFIC LISTING BY ID
+const getListingById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const findListing = await Listing.findById({ _id: id });
+    res.status(200).send(findListing);
+  } catch (error) {
+    res.status(404).send({
+      message: "Something went wrong",
+      error: error,
+    });
+  }
+};
+
+// UPDATE LISTING BY ID
+const updateListing = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const updateListing = await Listing.findByIdAndUpdate(
+      _id,
+      ($set = {
+        companyName: req.body.companyName,
+        description: req.body.description,
+        images: req.body.images,
+        cuisineType: req.body.cuisineType,
+      }),
+      { new: true }
+    );
+    res.status(200).send(updateListing);
+  } catch (error) {
+    res.status(404).send({
+      message: "Cannot update listing",
+      error: error,
+    });
+  }
+};
+
+// DELETE LISTING BY ID
+const deleteListing = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const deleteListing = await Listing.findOneAndDelete({ _id: _id });
+    res.status(200).send({
+      message: "Listing deleted successfully",
+      "Deleted Listing": deleteListing,
+    });
+  } catch (error) {
+    res.status(404).send({
+      message: "Cannot delete listing",
+      error: error,
+    });
+  }
+};
+
 module.exports = {
   getAllListings,
   addListing,
+  getListingById,
+  updateListing,
+  deleteListing,
 };
--- API/Routes/Listings/index.js
+++ API/Routes/Listings/index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   getAllListings,
   addListing,
+  getListingById,
+  updateListing,
+  deleteListing,
 } = require("../../Controller/Listings/listings");
 
 const app = express.Router();
 
 app.get("/", getAllListings);
+app.get("/:id", getListingById);
 app.post("/", addListing);
+app.patch("/:id", updateListing);
+app.delete("/:id", deleteListing);
 
 module.exports = app;
